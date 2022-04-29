41a3539343b2ac115d8123db6c54c4d3487601a6
Rupesh Virani <rupeshvirani01@gmail.com>
2022-04-29 16:32:24 +0530

    Create Project Layout

--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,29 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'shop/index.html')
+
+def about(request):
+    return HttpResponse("We are at about")
+
+def contact(request):
+    return HttpResponse("We are at about")
+
+def tracker(request):
+    return HttpResponse("We are at tracker")
+
+def search(request):
+    return HttpResponse("We are at search")
+
+def productView(request):
+    return HttpResponse("We are at product view")
+
+def checkout(request):
+    return HttpResponse("We are at checkout")
+
+
+
+def about(request):
+    return HttpResponse("We are at about")
+
+
+
