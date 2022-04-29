4182fab15831bde306b32d08f0cd5669c22a45fe
Rupesh Virani <rupeshvirani01@gmail.com>
2022-04-29 21:09:41 +0530

    Django Admin & Adding Data To The Database

--- shop/models.py
+++ shop/models.py
@@ -5,4 +5,4 @@ class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
     desc = models.CharField(max_length=300)
-    pub_date = models.DateField()
+    pub_date = models.DateField(blank=True)
